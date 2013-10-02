3f2d29ed78ecc888b8f30318331a86215f7b2fa4
richrout <rich86@gmail.com>
2013-10-01 19:53:03 -0600

    Updated code to work with back/forward buttons

--- Source/LonelyPage.Core/LonelyPage.Core/Scripts/lonelypage.js
+++ Source/LonelyPage.Core/LonelyPage.Core/Scripts/lonelypage.js
@@ -2,55 +2,79 @@
 var lonely = (function () {
     function lonely() {
     }
+    lonely.init = function () {
+        lonely.registerLinks();
+
+        // On back (or forward) browser buttons, navigate
+        window.onpopstate = function (event) {
+            if (event.state) {
+                lonely.loadContent(event.state.href);
+            }
+        };
+    };
+
+    lonely.pushState = function (statedata, title, url) {
+        if (window.history && window.history.pushState) {
+            window.history.pushState(statedata, title, url);
+        } else {
+            // #todo implement our own stack with state data
+            window.location.hash = url;
+        }
+    };
+
+    lonely.loadContent = function (url, method) {
+        $.ajax({
+            url: url,
+            type: method || lonely.defaultMethod,
+            cache: false,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-LonelyPartialRequest", "true");
+                if (lonely.layout) {
+                    xhr.setRequestHeader("X-LonelyLayout", lonely.layout);
+                }
+            }
+        }).done(function (response, status, xhr) {
+            var isContinue = true;
+            if (lonely.responseCallback) {
+                isContinue = lonely.responseCallback.call(this, response, status, xhr);
+                isContinue = isContinue !== false;
+            }
+
+            if (isContinue) {
+                if ($(lonely.contentSelector).length) {
+                    $(lonely.contentSelector).html(response);
+                    lonely.pushState({ href: url }, url, url);
+                } else {
+                    throw new Error("Selector " + lonely.contentSelector + " not found in DOM. Please set your content your selector using lonely.contentSelector");
+                }
+            }
+
+            // re-register links
+            lonely.registerLinks();
+        });
+    };
+
     lonely.registerLinks = function () {
         $("a").off('click').click(function (e) {
             var target = $(e.currentTarget);
             var url = target.attr('href');
-            var method = target.data('method') || "get";
+            var method = target.data('method');
 
             if (target.data('lonely-ignore')) {
                 return true;
             }
 
-            $.ajax({
-                url: url,
-                type: method,
-                cache: false,
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader("X-LonelyPartialRequest", "true");
-                    if (lonely.layout) {
-                        xhr.setRequestHeader("X-LonelyLayout", lonely.layout);
-                    }
-                }
-            }).done(function (response, status, xhr) {
-                var isContinue = true;
-                if (lonely.responseCallback) {
-                    isContinue = lonely.responseCallback.call(this, response, status, xhr);
-                    isContinue = isContinue !== false;
-                }
-
-                if (isContinue) {
-                    if ($(lonely.contentSelector).length) {
-                        $(lonely.contentSelector).html(response);
-                        window.history.pushState(url, url, url);
-                    } else {
-                        throw new Error("Selector " + lonely.contentSelector + " not found in DOM. Please set your content your selector using lonely.contentSelector");
-                    }
-                }
-
-                // re-register links
-                lonely.registerLinks();
-            });
-
+            lonely.loadContent(url, method);
             return false;
         });
     };
     lonely.contentSelector = '#lonelyContent';
     lonely.layout = null;
+    lonely.defaultMethod = "get";
     lonely.responseCallback = null;
     return lonely;
 })();
 
 $(function () {
-    lonely.registerLinks();
+    lonely.init();
 });
--- Source/LonelyPage.Core/LonelyPage.Core/Scripts/lonelypage.ts
+++ Source/LonelyPage.Core/LonelyPage.Core/Scripts/lonelypage.ts
@@ -3,55 +3,80 @@
 class lonely {
     static contentSelector: string = '#lonelyContent';
     static layout: string = null;
+    static defaultMethod: string = "get";
     static responseCallback: Function = null;
+
+    static init() {
+        lonely.registerLinks();
+
+        // On back (or forward) browser buttons, navigate
+        window.onpopstate = (event) => {
+            if (event.state) {
+                lonely.loadContent(event.state.href);
+            }
+        };
+    }
+    
+    static pushState(statedata: any, title: string, url?: string) {
+        if (window.history && window.history.pushState) {
+            window.history.pushState(statedata, title, url);
+        }
+        else {
+            // #todo implement our own stack with state data
+            window.location.hash = url;
+        }
+    }
+
+    static loadContent(url: string, method?: string ) {
+        $.ajax({
+            url: url,
+            type: method || lonely.defaultMethod,
+            cache: false,
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader("X-LonelyPartialRequest", "true");
+                if (lonely.layout) {
+                    xhr.setRequestHeader("X-LonelyLayout", lonely.layout);
+                }
+            }
+        })
+            .done(function (response, status, xhr) {
+                var isContinue = true;
+                if (lonely.responseCallback) {
+                    isContinue = lonely.responseCallback.call(this, response, status, xhr);
+                    isContinue = isContinue !== false;
+                }
+
+                if (isContinue) {
+                    if ($(lonely.contentSelector).length) {
+                        $(lonely.contentSelector).html(response);
+                        lonely.pushState({ href: url }, url, url);
+                    }
+                    else {
+                        throw new Error("Selector " + lonely.contentSelector + " not found in DOM. Please set your content your selector using lonely.contentSelector");
+                    }
+                }
+
+                // re-register links
+                lonely.registerLinks();
+            });
+    }
 
     static registerLinks() {
         $("a").off('click').click(function (e) {
             var target = $(e.currentTarget);
             var url = target.attr('href');
-            var method = target.data('method') || "get";
+            var method = target.data('method');
 
             if (target.data('lonely-ignore')) {
                 return true;
             }
 
-            $.ajax({
-                url: url,
-                type: method,
-                cache: false,
-                beforeSend: function (xhr) {
-                    xhr.setRequestHeader("X-LonelyPartialRequest", "true");
-                    if (lonely.layout) {
-                        xhr.setRequestHeader("X-LonelyLayout", lonely.layout);
-                    }
-                }
-            })
-                .done(function (response, status, xhr) {
-                    var isContinue = true;
-                    if (lonely.responseCallback) {
-                        isContinue = lonely.responseCallback.call(this, response, status, xhr);
-                        isContinue = isContinue !== false;
-                    }
-
-                    if (isContinue) {
-                        if ($(lonely.contentSelector).length) {
-                            $(lonely.contentSelector).html(response);
-                            window.history.pushState(url, url, url);
-                        }
-                        else {
-                            throw new Error("Selector " + lonely.contentSelector + " not found in DOM. Please set your content your selector using lonely.contentSelector");
-                        }
-                    }
-
-                    // re-register links
-                    lonely.registerLinks();
-                });
-
+            lonely.loadContent(url, method);
             return false;
         });
     }
 }
 
 $(function () {
-    lonely.registerLinks();
+    lonely.init();
 });
